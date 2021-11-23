e87a5a4c0e50f9a9cac39b7c2ea243100f946889
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-23 18:07:27 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let num3 = 11
+let num4 = 7
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random(1) + 1)
+
+let mod = num3 % num4
+
+let max = Math.max(1,5,3,7,4,1,7,5,2,20)
+
+console.log(mod)
